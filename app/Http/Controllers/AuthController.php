1dea12d4b0f3b5ce675dbf06fec3737e7ec16dd7
Karim <soul.kim3@gmail.com>
2017-10-31 16:28:20 +0200

    update auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,18 +5,14 @@
  * Date: 9/13/2017
  * Time: 12:07 PM
  */
-
 namespace App\Http\Controllers;
-
 use App\Providers\ApiRequest;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Mail;
 class AuthController extends BaseController
 {
     public function __construct(){
         parent::__construct();
     }
-
     public function register(Request $request){
         if($request->ajax()) {
             try{
@@ -48,17 +44,12 @@ class AuthController extends BaseController
         }
         return $this->view("auth.register");
     }
-
     public function login(Request $request){
-
         try{
             if( ! $request->secure() )
                 return redirect()->secure('/auth/login' );
-
         }catch (\Exception $exception){
-
         }
-
         if($request->ajax()) {
             try{
                 if($request->input('logged')){
@@ -68,7 +59,6 @@ class AuthController extends BaseController
                         "success" => "تم تسجيل الدخول بنجاح"
                     ]);
                 }
-
                 $username = $request->input("username");
                 $password = $request->input("password");
                 $api = new ApiRequest();
@@ -79,7 +69,6 @@ class AuthController extends BaseController
                     ]);
                 }elseif(count($result) > 0){
                     $request->session()->forget('user_info'); //reset
-
                     $request->session()->put('user_info', $result[0]);
                     //print_r($request->session()->all());
                     return response()->json([
@@ -98,20 +87,9 @@ class AuthController extends BaseController
         }
         return $this->view("auth.login");
     }
-
     public function reset(Request $request){
         return $this->view("auth.reset");
     }
-
-
-    public function resetByToken(Request $request){
-        $this->data["token"] = $request -> get("token");
-        if(!isset($this->data["token"]) or empty($this->data["token"])){
-            return redirect()->to('/');
-        }
-        return $this->view("auth.resetByToken");
-    }
-
     public function resetPassword(Request $request)
     {
         $hash = $request->get('hash');
@@ -119,16 +97,14 @@ class AuthController extends BaseController
         $email = $request->get('email');
         //  $username = $request->get('username');
         $emailname = explode('@', $request->get('email'));
-
         $username = $emailname[0];
-
         if ($hash && $token) {
             // Data to be used on the email view
             $data = array('token' => $token,
                 'user' => $username,
                 'messagebodysection1' => ' يرجى النقر على الوصله ادناه او نسخها ولصقها في المتصفح
                                 لتتمكن من وضع كلمة السر الخاصة بك',
-                'messagebodysection2' => '<a href="http://awaan.ae/auth/resetbytoken?token='. $token.'">الرابط</a>
+                'messagebodysection2' => '<a href="http://awaan.ae/?token='. $token.'">الرابط</a>
                         و شكرا'
             );
             // Send the welcome email
@@ -140,7 +116,6 @@ class AuthController extends BaseController
         }
         return response()->json(['sent' =>'result: ' . $result]);
     }
-
     public function logout(Request $request){
         $request->session()->flush();
         return redirect()->to('/');

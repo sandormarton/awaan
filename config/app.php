688f84a4a6600e27b880a0c54fbe066d86838e99
Hany alsamman <hany.alsamman@gmail.com>
2017-11-07 14:52:49 +0200

    Add Folklore library to compress images
    Signed-off-by: Hany alsamman <hany.alsamman@gmail.com>

--- config/app.php
+++ config/app.php
@@ -139,6 +139,11 @@ return [
                 App\Providers\AuthServiceProvider::class,
                 App\Providers\EventServiceProvider::class,
                 App\Providers\RouteServiceProvider::class,
+
+                /**
+                 *  Custom
+                 */
+                Folklore\Image\ImageServiceProvider::class,
         ],
         /*
           |--------------------------------------------------------------------------
@@ -183,6 +188,7 @@ return [
                 'Validator' => Illuminate\Support\Facades\Validator::class,
                 'View' => Illuminate\Support\Facades\View::class,
                 'HTML' => Illuminate\Html\HtmlFacade::class,
-                'Functions' => App\Helpers\Functions::class
+                'Functions' => App\Helpers\Functions::class,
+                'Image' => Folklore\Image\Facades\Image::class,
         ],
 ];

b3e86b5349a9c654f607eb773c461f19dc83c83b
Hany alsamman <hany.alsamman@gmail.com>
2018-01-18 16:44:54 +0200

    Dynamic year in footer

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -7,7 +7,7 @@
                 <img src="{{asset('images/logo.png')}}" alt="awaan logo" class="img-responsive center-block" /><a class="terms-footer"  target="_blank" href="http://www.dmi.gov.ae/privacy.asp?pgTitle=terms&ChannelID=10&lang=ar">{{ trans('content.whole.terms') }}</a><a class="footer-app-href" href="/app">{{ trans('content.app.apps') }}</a>
             </div>
             <div class="col-md-6 col-sm-6 text-awaan-sm">
-                <span>Awaan &copy; 2017</span>
+                <span>Awaan &copy; {{date('Y')}}</span>
                 <a href="https://www.facebook.com/OnAwaan/" target="_blank"><h4 style="display: none">facebook page</h4><i class="fa fa-facebook"></i></a>
                 <a href="https://twitter.com/OnAwaan" target="_blank"><h4 style="display: none">twitter page</h4><i class="fa fa-twitter"></i></a>
             </div>

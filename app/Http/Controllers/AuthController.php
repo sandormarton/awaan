cad0f5f7428a3dd64eadf015b5fe5a584f73fbf6
Hany alsamman <hany.alsamman@gmail.com>
2018-03-14 16:46:10 +0200

    Fix gender misstype

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,7 +5,9 @@
  * Date: 9/13/2017
  * Time: 12:07 PM
  */
+
 namespace App\Http\Controllers;
+
 use App\Providers\ApiRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
@@ -14,6 +16,7 @@ class AuthController extends BaseController
     public function __construct(){
         parent::__construct();
     }
+
     public function register(Request $request){
         if($request->ajax()) {
             try{
@@ -21,7 +24,7 @@ class AuthController extends BaseController
                 $result = $api->register([
                     "firstname" => $request->input("firstname"),
                     "lastname" => $request->input("lastname"),
-                    "gender" => $request->input("Gender"),
+                    "gender" => $request->input("gender"),
                     "email" => $request->input("email"),
                     "birthday" => $request->input("birthday"),
                     "username" => $request->input("username"),
@@ -45,12 +48,17 @@ class AuthController extends BaseController
         }
         return $this->view("auth.register");
     }
+
     public function login(Request $request){
+
         try{
             if( ! $request->secure() )
                 return redirect()->secure('/auth/login' );
+
         }catch (\Exception $exception){
+
         }
+
         if($request->ajax()) {
             try{
                 if($request->input('logged')){
@@ -60,6 +68,7 @@ class AuthController extends BaseController
                         "success" => "تم تسجيل الدخول بنجاح"
                     ]);
                 }
+
                 $username = $request->input("username");
                 $password = $request->input("password");
                 $api = new ApiRequest();
@@ -70,6 +79,7 @@ class AuthController extends BaseController
                     ]);
                 }elseif(count($result) > 0){
                     $request->session()->forget('user_info'); //reset
+
                     $request->session()->put('user_info', $result[0]);
                     //print_r($request->session()->all());
                     return response()->json([
@@ -88,6 +98,7 @@ class AuthController extends BaseController
         }
         return $this->view("auth.login");
     }
+
     public function reset(Request $request){
         return $this->view("auth.reset");
     }
@@ -108,14 +119,16 @@ class AuthController extends BaseController
         $email = $request->get('email');
         //  $username = $request->get('username');
         $emailname = explode('@', $request->get('email'));
+
         $username = $emailname[0];
+
         if ($hash && $token) {
             // Data to be used on the email view
             $data = array('token' => $token,
                 'user' => $username,
                 'messagebodysection1' => ' يرجى النقر على الوصله ادناه او نسخها ولصقها في المتصفح
                                 لتتمكن من وضع كلمة السر الخاصة بك',
-                'messagebodysection2' => '<a href="https://awaan.ae/auth/resetbytoken?token='. $token.'">الرابط</a>
+                'messagebodysection2' => '<a href="http://awaan.ae/auth/resetbytoken?token='. $token.'">الرابط</a>
                         و شكرا'
             );
             // Send the welcome email
@@ -127,6 +140,7 @@ class AuthController extends BaseController
         }
         return response()->json(['sent' =>'result: ' . $result]);
     }
+
     public function logout(Request $request){
         $request->session()->flush();
         return redirect()->to('/');
